87c06120da70ec6b002e3e342c9d0e24fdc064fb
PC wonton <wontonking123@outlook.com>
2022-08-31 15:29:05 +0200

    ocver

--- backend/middleware/password-validator.js
+++ backend/middleware/password-validator.js
@@ -8,9 +8,7 @@ passwordSchema
 .is().digits(2)
 
 module.exports = (req, res, next) => {
-    console.log(req.body.password)
     if (passwordSchema.validate(req.body.password)){
-        console.log(passwordSchema.validate(req.body.password))
         next()
     }else{
         res.status(400).json({message:'mdp a echoue pour x raison:'+passwordSchema.validate(req.body.password,{list:true})})
